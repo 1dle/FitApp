45fb0cfe3e93c4161b520fe75479327f61a58f1e
Bence Deli <delibnc@gmail.com>
2020-03-17 14:49:03 +0100

    ViewModel for Diary Fragment (live data)

--- app/src/main/java/com/undef/fitapp/ui/diary/DiaryFragment.kt
+++ app/src/main/java/com/undef/fitapp/ui/diary/DiaryFragment.kt
@@ -4,16 +4,65 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.undef.fitapp.R
+import kotlinx.android.synthetic.main.fragment_diary.*
 
 class DiaryFragment : Fragment() {
+
+    private lateinit var diaryViewModel: DiaryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        /*return inflater.inflate(R.layout.fragment_diary, container, false)*/
+        diaryViewModel = ViewModelProviders.of(this).get(DiaryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_diary, container, false)
+
+
+        val liveDailyStats = mutableListOf<Pair<TextView, LiveData<String>>>(
+            // Pair ( UI element name , property name in ViewModel )
+            Pair(root.findViewById(R.id.tvConsumed), diaryViewModel.consumedText),
+            Pair(root.findViewById(R.id.tvBurned), diaryViewModel.burnedText),
+            Pair(root.findViewById(R.id.tvRemaining), diaryViewModel.remainingText)
+        )
+
+        liveDailyStats.forEach { pit ->
+            pit.second.observe(viewLifecycleOwner, Observer {
+                pit.first.text = it
+            })
+        }
+
+        //test live data
+        /*
+        liveDailyStats[0].first.setOnClickListener {
+            diaryViewModel.changeConsumedText();
+        }*/
+
+        /*
+        // the forEach is same as these lines but shorter
+        // Consumed kcals from viewmodel
+        val tvConsumed: TextView = root.findViewById(R.id.tvConsumed)
+        diaryViewModel.consumedText.observe(viewLifecycleOwner, Observer {
+            tvConsumed.text = it
+        })
+        //Burned kcals
+        val tvBurned: TextView = root.findViewById(R.id.tvBurned)
+        diaryViewModel.burnedText.observe(viewLifecycleOwner, Observer {
+            tvBurned.text = it
+        })
+        //Remaining kcals
+        val tvRemaining: TextView = root.findViewById(R.id.tvRemaining)
+        diaryViewModel.remainingText.observe(viewLifecycleOwner, Observer {
+            tvRemaining.text = it
+        })*/
+
+        return root
     }
 }
---
+++ app/src/main/java/com/undef/fitapp/ui/diary/DiaryViewModel.kt
@@ -0,0 +1,27 @@
+package com.undef.fitapp.ui.diary
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DiaryViewModel : ViewModel() {
+    /* test
+    fun changeConsumedText() {
+        _consumedText.apply { value = "asd" }
+    }*/
+
+    private val _consumedText = MutableLiveData<String>().apply {
+        value = "Consumed: 0 kcals";
+    }
+    private val _burnedText = MutableLiveData<String>().apply {
+        value = "Burned: 0 kcals";
+    }
+    private val _remainingText = MutableLiveData<String>().apply {
+        value = "Remaining: 0 kcals";
+    }
+
+    val consumedText: LiveData<String> = _consumedText;
+    val burnedText: LiveData<String> = _burnedText;
+    val remainingText: LiveData<String> = _remainingText;
+
+}
