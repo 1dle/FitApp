28dd3a3455dba4ddeed246a84f9df0b9d01fe90a
Bence Deli <delibnc@gmail.com>
2020-03-18 14:20:19 +0100

    MealEditActivity UI

---
+++ app/src/main/java/com/undef/fitapp/ui/diary/EditMealActivity.kt
@@ -0,0 +1,13 @@
+package com.undef.fitapp.ui.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.undef.fitapp.R
+
+class EditMealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_meal)
+    }
+}
